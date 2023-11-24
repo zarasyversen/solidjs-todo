e88d8e4f172cd2338d622ceb5194326431237af4
Zara Syversen <zara.syversen@ninetech.com>
2023-11-24 13:21:23 +0100

    Add search of todoslist

--- src/components/TodosList.tsx
+++ src/components/TodosList.tsx
@@ -9,12 +9,37 @@ function TodosList(props: {
     addTodo: (newTitle: string) => void
     completeTodo: (id: number) => void
   }) {
+  
+  const [filteredTodos, setFilteredTodos] = createSignal(props.todos);
   const [isSearching, setIsSearching] = createSignal(false);
-  const toggleSearch = () => setIsSearching(!isSearching());
-  const [noResults, toggleNoResults] = createSignal(false);
+  const [noResults, setNoResults] = createSignal(false);
+  const toggleSearch = () => {
+    setIsSearching(!isSearching());
+    setFilteredTodos(props.todos);
+    setNoResults(false);
+  };
+
+  const searchTodos = (searchString: string) => {
+    const searchTerm = searchString;
+    if (searchTerm !== '') {
+      const matchedTodos = props.todos.filter((item) => {
+        return item.title.toLowerCase().indexOf(searchTerm.toLowerCase()) !== -1;
+      });
+
+      if (matchedTodos.length > 0) {
+        setFilteredTodos(matchedTodos);
+        setNoResults(false);
+      } else {
+        setFilteredTodos([]);
+        setNoResults(true);
+      }
+    } else {
+      setFilteredTodos(props.todos);
+      setNoResults(false);
+    }
+  };
 
   const [newTitle, setTitle] = createSignal("");
-
   const addTodo = (e: SubmitEvent) => {
     e.preventDefault();
     if (newTitle() === "") {
@@ -31,28 +56,6 @@ function TodosList(props: {
  
   return (
     <>
-    <div class="topContainer">
-      <h2>Tasks</h2>
-      <button
-        type="button"
-        class="button icon-button"
-        onClick={toggleSearch}
-      >Find</button>
-      {isSearching() &&
-        <form class="todoSearch">
-          <label for="todoSearch" class="sr-only" />
-          <input
-            type="text"
-            class="input-text todoSearchInput"
-            id="todoSearch"
-            placeholder="Search for task..."
-          />
-          {noResults() &&
-            <p>Sorry, no results. Maybe add a new item?</p>
-          }
-        </form>
-      }
-    </div>
       <div class="addTodo" >
         <form onSubmit={addTodo} class="addTodoForm">
           <label for="new-todo" class="addTodoForm__label">Add a new task</label>
@@ -68,12 +71,36 @@ function TodosList(props: {
           <button class="button addTodoForm__button"> Add </button>
         </form>
       </div>
-      <For each={props.todos}>
-        {(todo) => (
-          <TodoListItem todo={todo} completeItem={completingTodo} />
-        )}
-      </For>
-
+      <div class="topContainer">
+        <h2>Tasks</h2>
+        <button
+          type="button"
+          class="button icon-button"
+          onClick={toggleSearch}
+        >Find</button>
+        {isSearching() &&
+          <form class="todoSearch">
+            <label for="todoSearch" class="sr-only" />
+            <input
+              type="text"
+              class="input-text todoSearchInput"
+              id="todoSearch"
+              onInput={(e) => searchTodos(e.currentTarget.value)}
+              placeholder="Search for task..."
+            />
+            {noResults() &&
+              <p>Sorry, no results. Maybe add a new item?</p>
+            }
+          </form>
+        }
+      </div>
+      <ul>
+        <For each={filteredTodos()}>
+          {(todo) => (
+            <TodoListItem todo={todo} completeItem={completingTodo} />
+          )}
+        </For>
+      </ul>
     </>
   )
 }

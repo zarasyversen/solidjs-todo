66dd398dc7d7304f02671b972f4de51d6c8ae6eb
Zara Syversen <zara.syversen@ninetech.com>
2023-11-24 13:37:35 +0100

    Tidy up counter

--- src/components/counter/Counter.tsx
+++ src/components/counter/Counter.tsx
@@ -2,10 +2,11 @@ import { Show } from "solid-js";
 import { TodoItem } from "../../types/TodoItem";
 
 function Counter(props: {todos: TodoItem[]}) {
+    const completedTodos = (() => props.todos.filter((todo: TodoItem) => todo.completed === true).length);
     return (
         <Show when={props.todos.length > 0}>
             <p class="counter">
-                Hey, you have completed {props.todos.filter((todo: TodoItem) => todo.completed === true).length} of {props.todos.length} tasks!
+                Hey, you have completed {completedTodos()} of {props.todos.length} tasks!
             </p>
         </Show>
     )
